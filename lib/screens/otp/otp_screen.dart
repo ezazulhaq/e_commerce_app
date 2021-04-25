d265bfd1a0d99096284b4b8ef255210ec7d79d70
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-04-26 02:22:45 +0530

    first commit

---
+++ lib/screens/otp/otp_screen.dart
@@ -0,0 +1,19 @@
+// @dart = 2.9
+import 'package:flutter/material.dart';
+import 'package:shop_app/size_config.dart';
+
+import 'components/body.dart';
+
+class OtpScreen extends StatelessWidget {
+  static String routeName = "/otp";
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OTP Verification"),
+      ),
+      body: Body(),
+    );
+  }
+}
---
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,17 @@
+// @dart = 2.9
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class SignUpScreen extends StatelessWidget {
+  static String routeName = "/sign_up";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up"),
+      ),
+      body: Body(),
+    );
+  }
+}
